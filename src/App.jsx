c7598d29138f85d57e9b5ce9bde1c24e64d094ee
Lakshay Bhatia <136612476+lakshayb30@users.noreply.github.com>
2024-11-26 17:04:54 +0530

    UI fixes

--- src/App.jsx
+++ src/App.jsx
@@ -53,22 +53,14 @@ export default function App() {
   /* function to display todos */
   function Todo(props){
     return (
-      <div style={{display:'flex' ,alignItems:'centre', justifyContent:'space-between'}}>
         <div style={{display:'flex', alignItems:'center',marginBottom:'2vh'}}>
-          <input style={{marginRight:'2vw', width:'2vh', height:'2vh',}} type="checkbox" value=
-          {props.done}/>
-          <span >{props.description}</span>    
+          <input style={{marginRight:'2vw', width:'2vh', height:'2vh'}} type="checkbox" value={props.done}/>
+          <span style={{fontSize:18}}>{props.description}</span>
+          <img onClick={() => deleteTODO(props.index)} style={{marginLeft:'2vw',height:24}} src="./delete.png"/>
+          <img onClick={() => editTODO(props.index)} style={{marginLeft:'2vw',height:24}} src="./edit.png" />
         </div>
-        <div id = 'div1'>
-          <svg onClick={() => deleteTODO(props.index)} style={{marginLeft:'2vw'}} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="undefined"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
-          <svg onClick={() => editTODO(props.index)} style={{marginLeft:'2vw'}} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="undefined"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
-        </div>
-      </div>
-      
     )
   }
-
-  /*XML i.e. face of the web app */
   return (
     <div className='whole'>
       <div id='form'>
